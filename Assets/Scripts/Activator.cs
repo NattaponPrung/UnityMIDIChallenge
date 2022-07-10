951e2df4279ec3a2ce463bdded6a33b66e8aeb37
NattaponPrung <96724290+NattaponPrung@users.noreply.github.com>
2022-07-10 16:03:01 +0700

    save 1

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    SpriteRenderer sr;
+    public KeyCode key;
+    bool active = false;
+    GameObject note;
+    Color old ;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    
+    void Start()
+    {
+        old=sr.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(key))
+         StartCoroutine(Pressed());
+
+        if(Input.GetKeyDown(key)&&active){
+            Destroy(note);
+            AddScore();
+            active=false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        active=true;
+        if(col.gameObject.tag=="Note")
+        note = col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        active=false;
+    }
+
+    void AddScore()
+    {
+        PlayerPrefs.SetInt("Score",PlayerPrefs.GetInt("Score")+100);
+    }
+
+    IEnumerator Pressed()
+    {
+        sr.color = new Color(0,0,0);
+        yield return new WaitForSeconds(0.1f);
+        sr.color = old;
+    }
+}
